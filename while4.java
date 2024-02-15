d2d206de19273f9e0514a2e2bade565ce78524d5
duban77 <159207588+duban77@users.noreply.github.com>
2024-02-15 09:28:06 -0500

    Create while4.java}

---
+++ while4.java
@@ -0,0 +1,12 @@
+package com.mycompany.piper;
+import java.util.Scanner;
+public class while4 {
+    public static void main (String[] args){
+        int i = 0;
+        while(true) {
+            i++;
+            System.out.println("Valor de i: " +i);
+            if (i==9) {break;}   
+        }
+    }
+}
