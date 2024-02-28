f1ebaddc4f6ffbe974c181541fa5c5c2714d2a77
duban77 <159207588+duban77@users.noreply.github.com>
2024-02-28 13:02:49 -0500

    Create dowhile1.cpp

---
+++ dowhile1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n=10;
+cout<<"Programa que va desde 10 hasta 0.\n"<<endl;
+do{ cout<<n<<" ";
+n--;
+} while (n>=0); cout<<endl;
+system("PAUSE");
+    return 0;
+}
