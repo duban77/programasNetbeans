b2268026cfe657aa5768548ccdf0a7d16443c457
duban77 <159207588+duban77@users.noreply.github.com>
2024-03-07 09:17:43 -0500

    Update POO2.java

--- POO2.java
+++ POO2.java
@@ -27,5 +27,48 @@ public class Computador {
     System.out.println("EL tamaño de la torre es: "+torre.tamaño);
     
     
+    Computador montor = new Computador();
+    torre.color="blanco";
+    torre.marca="Asus";
+    torre.tamaño="grande";
+    torre.resolucion=" 2560 x 1080 píxeles";
+    torre.hz =  240;
+    
+    
+    System.out.println("El color de la monitor es: "+torre.color);
+    System.out.println("La marca de la monitor es: "+torre.marca);
+    System.out.println("EL tamaño de la monitor es: "+torre.tamaño);
+    System.out.println("La resolucion de la monitor es: "+torre.resolucion);
+    System.out.println("EL hz de la monitor es: "+torre.hz);
+    
+    Computador teclado = new Computador();
+    torre.color="universal";
+    torre.marca="Loditech";
+    torre.tamaño="Pequeño";
+    torre.tipo =  "Mecanico";
+    
+    
+    System.out.println("El color de la teclado es: "+torre.color);
+    System.out.println("La marca de la teclado es: "+torre.marca);
+    System.out.println("EL tamaño de la teclado es: "+torre.tamaño);
+    System.out.println("El tipo de la teclado es: "+torre.tipo);
+    
+     Computador raton = new Computador();
+    torre.color="Azul";
+    torre.marca="Loditech";
+    torre.tamaño="Pequeño";
+    torre.dpi =  800;
+    torre.botones =  6 ;
+    
+    
+    System.out.println("El color de la raton es: "+torre.color);
+    System.out.println("La marca de la raton es: "+torre.marca);
+    System.out.println("EL tamaño de la raton es: "+torre.tamaño);
+    System.out.println("El dpi del raton es: "+torre.dpi);
+    System.out.println("Los botones del raton es: "+torre.botones);
+    
+    
+    
+    
     }    
 }
